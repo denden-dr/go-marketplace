588a981d2ccaea574ea1e54f6656de86bc206285
denden-dr <deni.rianto0208@gmail.com>
2026-04-13 10:32:34 +0700

    chore: wire cart and order services and update mocks

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -7,6 +7,8 @@ import (
 	"go-shop-yourself/internal/product"
 	"go-shop-yourself/internal/user"
 	"go-shop-yourself/internal/wallet"
+	"go-shop-yourself/internal/cart"
+	"go-shop-yourself/internal/order"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -18,6 +20,8 @@ func SetupRoutes(
 	merchantHandler *merchant.MerchantHandler,
 	productHandler *product.ProductHandler,
 	walletHandler *wallet.WalletHandler,
+	cartHandler *cart.CartHandler,
+	orderHandler *order.OrderHandler,
 	jwtSecret string,
 ) {
 	// Create API base group
@@ -54,4 +58,31 @@ func SetupRoutes(
 	wallets.Get("/", walletHandler.GetWallet)
 	wallets.Get("/history", walletHandler.GetHistory)
 	wallets.Post("/withdraw", walletHandler.Withdraw)
+
+	// V1 Routes
+	v1 := api.Group("/v1")
+
+	// User features (Cart & Orders)
+	userV1 := v1.Group("/user")
+	
+	// Cart
+	cartRoutes := userV1.Group("/cart")
+	cartRoutes.Get("/", cartHandler.GetCart)
+	cartRoutes.Post("/", cartHandler.AddToCart)
+	cartRoutes.Put("/:id", cartHandler.UpdateCartItem)
+	cartRoutes.Delete("/:id", cartHandler.RemoveFromCart)
+	cartRoutes.Delete("/", cartHandler.ClearCart)
+
+	// Orders
+	orderRoutes := userV1.Group("/orders")
+	orderRoutes.Post("/", orderHandler.Checkout)
+	orderRoutes.Get("/:id", orderHandler.GetOrderDetail)
+	orderRoutes.Put("/:id/cancel", orderHandler.UserCancelOrder)
+	orderRoutes.Post("/:id/appeal", orderHandler.UserAppealOrder)
+
+	// Merchant features (Orders)
+	merchantV1 := v1.Group("/merchant")
+	merchantOrders := merchantV1.Group("/orders")
+	merchantOrders.Put("/:id/cancel", orderHandler.MerchantCancelOrder)
+	merchantOrders.Put("/:id/status", orderHandler.MerchantUpdateStatus)
 }
